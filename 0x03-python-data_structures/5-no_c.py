46f442a347a3250cbc13921ca910dc5a730aca47
fim4re <fimareyassine@gmail.com>
2024-01-08 15:38:13 -0800

    commit for the task

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -3,5 +3,5 @@ def no_c(my_string):
     t = ""
     for y in range(len(my_string)):
         if (my_string[y] != 'c' and my_string[y] != 'C'):
-            t += my_string[i]
+            t += my_string[y]
     return t
