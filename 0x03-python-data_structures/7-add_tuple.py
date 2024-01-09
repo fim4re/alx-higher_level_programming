3ff8bca6fa29e1cf0c1eaaeea993d77532a9b14e
fim4re <fimareyassine@gmail.com>
2024-01-08 16:10:46 -0800

    commit for the task

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -5,5 +5,5 @@ def add_tuple(tuple_a=(), tuple_b=()):
     a1 += tuple_a[1]
     b0 += tuple_b[0]
     b1 += tuple_b[1]
-    tuple_n = (a0 + b0, a1 + b1)
+    tuple_n = tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1]
     return tuple_n
