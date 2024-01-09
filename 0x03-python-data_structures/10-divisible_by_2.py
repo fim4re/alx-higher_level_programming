ac98317159fc359f4b40aeadac12fe94b43866b7
fim4re <fimareyassine@gmail.com>
2024-01-08 18:16:34 -0800

    commit for the task

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    l = len(my_list)
     n_list = []
-    for y in range(l):
-        if my_list[y] % 2 == 0:
+    for y in my_list:
+        if (y % 2) == 0:
             n_list.append(True)
         else:
             n_list.append(False)
