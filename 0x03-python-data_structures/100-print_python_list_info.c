b715459d48c15346a814c3cf4244928c8e2c4f1b
fim4re <fimareyassine@gmail.com>
2024-01-09 15:08:19 -0800

    commit for the task

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,19 @@
+#include <Python.h>
+
+/**
+ * print_python_list_info - print info about python
+ * @p: python object list
+*/
+
+void print_python_list_info(PyObject *p)
+{
+	long int size = Py_Size(p);
+	int i, alloc;
+
+	alloc = ((PyListObject *)p)->allocated;
+
+	printf("[*] size of the Python List = %li\n", size);
+	printf("[*] Allocated = %d\n", alloc);
+	for (i = 0; i < size; i++)
+		printf("Element %i: %s\n", i, Py_TYPE(obj->ob_item[i])->tp_name);
+}
