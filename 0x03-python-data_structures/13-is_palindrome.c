62121bae554f34fca1b5cbba7e88758b22efb570
fim4re <fimareyassine@gmail.com>
2024-01-08 18:51:39 -0800

    commit for the task

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,30 @@
+#include "list.h"
+
+/**
+ * is_palindrome - checks if a singly list is a palidrome
+ * @head: pointer to pointer
+ * Return: 0 if a palidrome is not, 1 if yes
+*/
+int is_palindrome(listint_t **head)
+{
+	if (head == NULL || *head == NULL)
+		return (1);
+	return (aux_palind(head, *head));
+}
+/**
+ * aux_palind - know if is palindrome
+ * @head: head list
+ * @end: end list
+ * Return: 1 if aux palind
+*/
+int aux_palind(listint_t **head, listint_t *end)
+{
+	if (end == NULL)
+		return (1);
+	if (aux_palind(head, end->next) && (*head)->n == end->n)
+	{
+		*head = (*head)->next;
+		return (1);
+	}
+	return (0);
+}
