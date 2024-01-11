a12c1af9c768618945c5032bf41422ebe2a44b45
fim4re <fimareyassine@gmail.com>
2024-01-11 06:49:36 -0800

    commit for the task

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
     s = []
-    for l in matrix:
-        s.append([i**2 for i in l])
+    for n in matrix:
+        s.append([i**2 for i in n])
     return s
