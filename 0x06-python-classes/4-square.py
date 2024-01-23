0a0a204ff4343cc46fc0d448cbf08ce7033dd3c6
fim4re <fimareyassine@gmail.com>
2024-01-23 09:27:06 -0800

    commit for the task

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+"""class square."""
+
+
+class Square:
+    """Defines square."""
+
+    def __init__(self, size=0):
+        """new square.
+
+        Args:
+        size: size of the new square.
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """set/get size of the square."""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """return area of the square."""
+        return (self.__size * self.__size)
