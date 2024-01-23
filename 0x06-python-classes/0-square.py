b2893cdf8e2d3557bd4f705859843e904a24ae65
fim4re <fimareyassine@gmail.com>
2024-01-23 08:25:33 -0800

    commit for the task

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""Class square."""
+
+class Square:
+    """Defines square."""
+    pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Class Square."""
+
+class Square:
+    """Defines square."""
+
+    def __init__(self, size):
+        """New square.
+
+        Args:
+            size: size of the new square.
+        """
+        self.__size = size
