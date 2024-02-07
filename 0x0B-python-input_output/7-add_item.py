6d1559f58559ab54388468ed19fd24daa20a3eca
fim4re <fimareyassine@gmail.com>
2024-02-07 08:27:04 -0800

    commit for the task

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -6,8 +6,6 @@ import sys
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-#arg = list(sys.argv[1:])
-
 try:
     items_data = load_from_json_file("add_item.json")
 except FileNotFoundError:
