5ffdc658f9df5673c461079230202a70e2476b42
fim4re <fimareyassine@gmail.com>
2024-02-07 08:16:10 -0800

    commit for the task

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,16 +3,14 @@
 import sys
 
 
-if __name__ == "__main__":
-    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    load_from_json_file = \
-__import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-    arg = list(sys.argv[1:])
+arg = list(sys.argv[1:])
 
-    try:
-        items_data = load_from_json_file("add_item.json")
-    except Exception:
-        items_data = []
-        items_data.extend(arg)
-        save_to_json_file(items_data, "add_item.json")
+try:
+    items_data = load_from_json_file("add_item.json")
+except FileNotFoundError:
+    items_data = []
+items_data.extend(arg)
+save_to_json_file(items_data, "add_item.json")
