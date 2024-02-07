6cff0ccb87eda9250c0153c3247c2df4b56fbcb0
fim4re <fimareyassine@gmail.com>
2024-02-07 09:23:17 -0800

    commit for the task

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -10,7 +10,7 @@ class Student:
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
-        
+
     def to_json(self, attrs=None):
         """dictionary of the Student."""
         if (type(attrs) == list and
