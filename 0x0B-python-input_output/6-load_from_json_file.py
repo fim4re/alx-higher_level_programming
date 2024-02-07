7ba997c9662f43c0ba48166dcc757b133d148549
fim4re <fimareyassine@gmail.com>
2024-02-07 08:23:51 -0800

    commit for the task

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -5,5 +5,5 @@ import json
 
 def load_from_json_file(filename):
     """Python object from a JSON file."""
-    with open(filename, 'r') as f:
+    with open(filename) as f:
         return json.load(f)
