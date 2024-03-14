c7e5381688da4c58e5657355b88a3ac448558a49
fim4re <fimareyassine@gmail.com>
2024-03-13 18:17:57 -0700

    commit for the task

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,7 @@
+-- comments
+-- creates the database hbtn_0d_2 and the user user_0d_2
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+-- the user
+CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
+-- script grants SELECT privileges to a user
+GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
