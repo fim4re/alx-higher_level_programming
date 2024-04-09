48bf4c5f628e5434fa63b636afd3639aeffc0080
fim4re <fimareyassine@gmail.com>
2024-04-08 18:37:39 -0700

    commit for the task

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const cnt = process.argv.length;
+
+if (cnt === 2) {
+	console.log('No argument');
+} else if (cnt === 3) {
+	console.log('Argument found');
+} else {
+	console.log('Arguments found');
+}
