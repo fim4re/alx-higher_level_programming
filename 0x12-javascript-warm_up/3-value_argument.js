86ccc680322f0cfa9519e384af53bc09916f6e8c
fim4re <fimareyassine@gmail.com>
2024-04-08 18:57:31 -0700

    commit for the task

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const cnt = process.argv[2];
 if (cnt === undefined) {
-	console.log('No argument');
+  console.log('No argument');
 } else {
-	console.log(cnt);
+  console.log(cnt);
 }
