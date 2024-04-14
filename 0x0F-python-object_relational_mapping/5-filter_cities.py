cb70f2ea4f835a17bd0ea8d57afdf300f93352f7
fim4re <fimareyassine@gmail.com>
2024-04-14 23:35:48 +0000

    commit for the task

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+""" lists all states from the hbtn_0e_0_usa."""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    crs = db.cursor()
+
+    q = ("SELECT * FROM `cities` as `c` \
+                INNER JOIN `states` as `s` \
+                   ON `c`.`state_id` = `s`.`id` \
+                ORDER BY `c`.`id`")
+    crs.execute(q)
+
+    print(", ".join([ci[2] for cit in crs.fetchall() if ci[4] == sys.argv[4]]))
+    crs.close()
+    db.close()
