dcf91df64402c17c5bc2ab3774839af47ed7929e
fim4re <fimareyassine@gmail.com>
2024-04-15 01:09:41 +0000

    commit for the task

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -13,5 +13,5 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     sen = Session()
     for inst in (sen.query(State.name, City.id, City.name) \
-                              .filter(State.id == City.state_id):
+                              .filter(State.id == City.state_id)):
         print(inst[0] + ": (" + str(inst[1]) + ") " + inst[2])
