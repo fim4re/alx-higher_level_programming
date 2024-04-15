b0f48963f6eaa61df62b63444e832fc344e3bd20
fim4re <fimareyassine@gmail.com>
2024-04-15 02:09:33 +0000

    commit for the task

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -2,7 +2,7 @@
 """ file similar to model_state named model_city """
 
 import sys
-from model_state import State
+from model_state import Base, State
 from model_city import City
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -11,6 +11,7 @@ from sqlalchemy.orm import sessionmaker
 if __name__ == "__main__":
     engine = create_engine("mysql+mysqldb://{}:{}@localhost/{}"
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Base.metadata.create_all(engine)
     # create session
     Session = sessionmaker(bind=engine)
     session = Session()
