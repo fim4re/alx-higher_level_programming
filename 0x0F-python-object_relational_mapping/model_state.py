42f8d72740cd0a4861ecbe8432e1762a1f8f0428
fim4re <fimareyassine@gmail.com>
2024-04-15 02:03:52 +0000

    commit for the task

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -8,7 +8,7 @@ Base = declarative_base(metadata=my_meta_data)
 
 
 class State(Base):
-    """ Class with id and name of each state"""
+    """ id and name of each state"""
     __tablename__ = 'states'
     id = Column(Integer, unique=True, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
