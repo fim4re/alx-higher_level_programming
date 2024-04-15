6e25a5e32df27e2fd0df87a27b7620c807e7881a
fim4re <fimareyassine@gmail.com>
2024-04-15 01:06:53 +0000

    commit for the task

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -7,7 +7,7 @@ from sqlalchemy.ext.declarative import declarative_base
 
 class City(Base):
     """defines each city"""
-    __tablename__ = 'citie'
+    __tablename__ = 'cities'
     id = Column(Integer, unique=True, nullable=False, primary_key=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey("states.id"), nullable=False)
