2d8939bf2d4e9102ab1126e0b0d6065d8795e7fc
fim4re <fimareyassine@gmail.com>
2024-04-15 02:07:53 +0000

    commit for the task

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -2,7 +2,7 @@
 """ file similar to model_state named model_city """
 
 import sys
-from model_state import Base, State
+from model_state import State
 from model_city import City
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -14,7 +14,7 @@ if __name__ == "__main__":
     # create session
     Session = sessionmaker(bind=engine)
     session = Session()
-    for state, city in (session.query(State, City)
-                     .filter(City.state_id == State.id).order_by(City.id)):
+    for state, city in session.query(State, City)
+                     .filter(City.state_id == State.id).order_by(City.id):
         # print city and state
         print("{}: ({}) {}".format(state.name, city.id, city.name))
