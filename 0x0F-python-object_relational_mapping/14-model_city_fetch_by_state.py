09b189ef6d652269714c35f0195e880752572a10
fim4re <fimareyassine@gmail.com>
2024-04-15 02:22:18 +0000

    commit for the task

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -15,6 +15,6 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
     for state, city in session.query(State, City) \
-                     .filter(City.state_id == State.id).order_by(City.id):
+                      .filter(City.state_id == State.id).order_by(City.id):
         # print city and state
         print("{}: ({}) {}".format(state.name, city.id, city.name))
