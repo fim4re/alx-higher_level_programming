ee240b036f1e9ce26fef1bdc52d5684be91a0db4
fim4re <fimareyassine@gmail.com>
2024-04-25 16:52:09 -0700

    commit for the task

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" peak-finding algorithm."""
+
+
+def find_peak(list_of_integers):
+    """ Finds the peak in a list of ints """
+    if list_of_integers is None or list_of_integers == []:
+        return None
+
+    le = len(list_of_integers)
+    mid = int(length / 2)
+    lo = list_of_integers
+
+    if mid - 1 < 0 and mid + 1 >= le:
+        return lo[mid]
+    elif mid - 1 < 0:
+        return lo[mid] if lo[mid] > lo[mid + 1] else lo[mid + 1]
+    elif mid + 1 >= le:
+        return lo[mid] if i[mid] > lo[mid - 1] else lo[mid - 1]
+
+    if lo[mid - 1] < lo[mid] > lo[mid + 1]:
+        return lo[mid]
+
+    if lo[mid + 1] > lo[mid - 1]:
+        return find_peak(lo[mid:])
+    return find_peak(lo[:mid])
