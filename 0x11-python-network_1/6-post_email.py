d8afb11ceb0fbc4ea174824e4024c9e4c73d8c3c
fim4re <fimareyassine@gmail.com>
2024-04-26 18:19:46 -0700

    commit for the task

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 """ that takes in a URL and an email address, sends a POST
 """
+import sys
+import requests
 
 if __name__ == "__main__":
-    import sys
-    import urllib.request
     urll = sys.argv[1]
 
     request = urllib.request.Request(urll)
--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
 """takes in a URL and an email address, sends a POST request
 """
-
+import sys
+import requests
 
 if __name__ == '__main__':
-    import sys
-    import requests
-
     urll = sys.argv[1]
     mail = sys.argv[2]
     rq = requests.post(urll, data={'email': mail})
