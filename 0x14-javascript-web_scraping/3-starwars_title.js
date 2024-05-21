b10545c3eceeef20a16001f67a89bb0f847998dc
fim4re <fimareyassine@gmail.com>
2024-05-21 16:36:39 -0700

    commit for the task

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -2,7 +2,7 @@
 
 const request = require('request');
 const ep = process.argv[2];
-const url = 'https://swapi-api.alx-tools.com/api/films/'
+const url = 'https://swapi-api.alx-tools.com/api/films/';
 request(url + ep, function (error, response, body) {
   console.log(error || JSON.parse(body).title);
 });
