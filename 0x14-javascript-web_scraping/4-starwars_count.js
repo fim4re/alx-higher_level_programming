2f4947e7f44c8d4d00601c6a782fa2b20acc76fe
fim4re <fimareyassine@gmail.com>
2024-05-21 16:47:39 -0700

    commit for the task

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-const req = require('request');
-req(process.argv[2], function (error, response, body) {
+const request = require('request');
+request(process.argv[2], function (error, response, body) {
   if (!error) {
-    const res = JSON.parse(body).res;
-    console.log(res.reduce((count, movie) => {
+    const results = JSON.parse(body).results;
+    console.log(results.reduce((count, movie) => {
       return movie.characters.find((character) => character.endsWith('/18/'))
         ? count + 1
         : count;
