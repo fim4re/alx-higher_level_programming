075977474f2e9916e90a2d762bbfe7335476683b
fim4re <fimareyassine@gmail.com>
2024-05-21 17:19:03 -0700

    commit for the task

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -6,15 +6,15 @@ req.get(url, function (error, res, body) {
   if (error) {
     console.log(error);
   }
-  const ab = JSON.parse(body);
-  const dd = ab.characters;
+  const data = JSON.parse(body);
+  const dd = data.characters;
   for (const i of dd) {
     req.get(i, function (error, res, body1) {
       if (error) {
         console.log(error);
       }
-      const ab1 = JSON.parse(body1);
-      console.log(ab1.name);
+      const data1 = JSON.parse(body1);
+      console.log(data1.name);
     });
   }
 });
