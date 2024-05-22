83969e3b39d7cdcdd37b7aab2c78bc407f741028
fim4re <fimareyassine@gmail.com>
2024-05-21 17:01:21 -0700

    commit for the task

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -2,15 +2,15 @@
 const request = require('request');
 request(process.argv[2], function (error, response, body) {
   if (!error) {
-    const tod = JSON.parse(body);
-    const complet = {};
-    tod.forEach((todo) => {
-      if (todo.complet && complet[todo.userId] === undefined) {
-        complet[todo.userId] = 1;
-      } else if (todo.complet) {
-        complet[todo.userId] += 1;
+    const todos = JSON.parse(body);
+    const completed = {};
+    todos.forEach((todo) => {
+      if (todo.completed && completed[todo.userId] === undefined) {
+        completed[todo.userId] = 1;
+      } else if (todo.completed) {
+        completed[todo.userId] += 1;
       }
     });
-    console.log(complet);
+    console.log(completed);
   }
 });
