8a0ce058b6624eb822cf2b1f2304e9d097290463
fim4re <fimareyassine@gmail.com>
2024-05-21 17:22:38 -0700

    commit for the task

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -2,19 +2,19 @@
 
 const request = require('request');
 const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
-req.get(url, function (error, res, body) {
+request.get(url, function (error, res, body) {
   if (error) {
     console.log(error);
   }
-  const data = JSON.parse(body);
-  const dd = data.characters;
+  const d = JSON.parse(body);
+  const dd = d.characters;
   for (const i of dd) {
-    req.get(i, function (error, res, body1) {
+    request.get(i, function (error, res, body1) {
       if (error) {
         console.log(error);
       }
-      const data1 = JSON.parse(body1);
-      console.log(data1.name);
+      const d1 = JSON.parse(body1);
+      console.log(d1.name);
     });
   }
 });
